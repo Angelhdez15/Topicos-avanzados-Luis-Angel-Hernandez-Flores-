16bfb3d7dc7dc697aaf263d1716d8be43e79dccd
Angelhdez15 <luisangelhernandezflores604@gmail.com>
2025-05-20 11:12:56 -1200

    eliminacion de cafeteria

--- cafeteria/api-cafe/utils/imagen.js
+++
@@ -1,9 +0,0 @@
-function getFilePath(file) {
-    const filePath = file.path;
-    const fileSplit = filePath.split("\\");
-    return fileSplit.join("/"); // Convierte las barras invertidas en barras normales
-}
-
-module.exports = {
-    getFilePath
-};
